b93540c5192c207b4312109c6b8d4fdc031cd639
Konrad Dzwinel <kdzwinel@gmail.com>
2018-01-06 18:44:47 +0100

    Fix jsdoc and tests.

--- lighthouse-core/gather/gatherers/seo/font-size.js
+++ lighthouse-core/gather/gatherers/seo/font-size.js
@@ -150,7 +150,7 @@ function isNonEmptyTextNode(node) {
 class FontSize extends Gatherer {
   /**
    * @param {{driver: !Object}} options Run options
-   * @return {!Promise<{totalTextLength: number, failingTextLength: number, visitedTextLength: number, analyzedFailingTextLength: number, analyzedFailingNodesData: Array<{fontSize: number, textLength: number, node: Node, cssRule: WebInspector.CSSStyleDeclaration}>}>} font-size analysis
+   * @return {!Promise<{totalTextLength: number, failingTextLength: number, visitedTextLength: number, analyzedFailingTextLength: number, analyzedFailingNodesData: Array<{fontSize: number, textLength: number, node: Node, cssRule: SimplifiedStyleDeclaration}>}>} font-size analysis
    */
   afterPass(options) {
     const stylesheets = new Map();
@@ -235,3 +235,12 @@ class FontSize extends Gatherer {
 
 module.exports = FontSize;
 
+/**
+ * Simplified, for serializability sake, WebInspector.CSSStyleDeclaration
+ * @typedef {Object} SimplifiedStyleDeclaration
+ * @property {string} type
+ * @property {{startLine: number, startColumn: number}} range
+ * @property {{origin: string, selectors: Array<{text: string}>}} parentRule
+ * @property {string} styleSheetId
+ * @property {WebInspector.CSSStyleSheetHeader} stylesheet
+ */
